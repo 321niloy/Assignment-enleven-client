4d8634993c803d2e4c018ec8b0023340d1f456ad
321niloy <niloyn670@gmail.com>
2023-05-21 11:54:30 +0600

    redmefile

---
+++ README.md
@@ -0,0 +1,18 @@
+- At frist we open cmd and create react app and install all we need to work
+- like that react route , tailwind ,daisyui
+-  we create header section and footer section in main route
+- then we make a homepage component
+- then we create components for many pages and create nastet routing
+- we create useTitle hook and use all components
+- then we create a Provider for using context routing,its name AuthProvider
+-  then we use privite route for login festures etc
+
+
+## Features
+
+- Responsive design for mobile and desktop
+- Customizable color scheme
+- Tailwind
+- daisyui
+- react icons
+
--- src/pages/alltoys/Alltoys.jsx
+++ src/pages/alltoys/Alltoys.jsx
@@ -1,32 +1,28 @@
-import React, {  useState } from 'react';
+import React, {  useEffect, useState } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import useTitle from '../../usetitle/Usetitle';
 
 
 const Alltoys = () => {
     const alltoys = useLoaderData()
-    const [searchText, setSearchText] = useState("");
     useTitle('All TOys')
     
 
-    const handleSearch = () => {
-      fetch(`http://localhost:3000/searchtoysText/${searchText}`)
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-          setJobs(data);
-        });
-    };
-    
+ 
+
     return (
         <div className='mt-4 mb-4'>
              <div className="search-box p-2 text-center">
           <input
-            onChange={(e) => setSearchText(e.target.value)}
+           
             type="text"
             className="border rounded-lg border-gray-400 py-1 px-2 "
           />{" "}
-          <button className=' border-rose-400 p-2  rounded-xl text-white bg-pink-700' onClick={handleSearch}>Search</button>
+          <button className=' border-rose-400 p-2  rounded-xl text-white bg-pink-700' >Search</button>
+        </div>
+        <div className='flex mb-9'>
+          <button onClick={() => lowtohigh('lowto')}  className=' border-rose-400 p-2  rounded-xl text-white bg-pink-700 me-2'>low to High</button>
+          <button className=' border-rose-400 p-2  rounded-xl text-white bg-pink-700'>     High to low </button>
         </div>
 
             <div className="overflow-x-auto">
@@ -34,7 +30,7 @@ const Alltoys = () => {
     {/* head */}
     <thead>
       <tr>
-        
+        <th></th>
         <th>ToyName</th>
         <th>Subcategory</th>
         <th>Price</th>
@@ -46,16 +42,18 @@ const Alltoys = () => {
     
 
 {
-    alltoys.map(alltoy =>    <tr >
-        
-        <td className='bg-pink-200 text-slate-700'>{alltoy.toyname}</td>
-        <td className='bg-pink-200 text-slate-700'>{alltoy.subcategory}</td>
-        <td className='bg-pink-200 text-slate-700'>{alltoy.price}tk</td>
-        <td className='bg-pink-200 text-slate-700'>{alltoy.quantity}</td>
-        <td className='bg-pink-200 '><Link to={`/alltoys/${alltoy._id}`} className='bg-pink-600 p-3 rounded-xl text-white font-extrabold'>View Details</Link></td>
-      </tr>)
-   }
-    
+   alltoys.map((alltoy , index)=>    <tr >
+   <td className='bg-pink-200 text-slate-700'>{index+1}</td>
+   <td className='bg-pink-200 text-slate-700'>{alltoy.toyname}</td>
+   <td className='bg-pink-200 text-slate-700'>{alltoy.subcategory}</td>
+   <td className='bg-pink-200 text-slate-700'>{alltoy.price}tk</td>
+   <td className='bg-pink-200 text-slate-700'>{alltoy.quantity}</td>
+   <td className='bg-pink-200 '><Link to={`/alltoys/${alltoy._id}`} className='bg-pink-600 p-3 rounded-xl text-white font-extrabold'>View Details</Link></td>
+ </tr>)
+
+}
+   
+
 
     
     </tbody>
